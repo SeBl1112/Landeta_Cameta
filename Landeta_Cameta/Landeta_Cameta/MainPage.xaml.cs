1eff607cf1631dfb73a1f2fbd068b14c90c1e416
santiago <santiagolandeta567@gmail.com>
2020-10-15 19:32:50 -0500

    Initial commit

---
+++ Landeta_Cameta/Landeta_Cameta/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Landeta_Cameta
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            TakePhoto.Clicked += TakePhoto_Clicked;
+        }
+
+        private async void TakePhoto_Clicked(object sender, EventArgs e)
+        {
+            var cameraOptions =
+                new Plugin.Media.Abstractions.StoreCameraMediaOptions();
+            cameraOptions.SaveToAlbum= true;
+            var photo =
+                await Plugin.Media.CrossMedia.Current
+                .TakePhotoAsync(cameraOptions);
+
+            if (photo != null)
+            {
+                Photo.Source = ImageSource.FromStream(() =>
+                {
+                    return photo.GetStream();
+                });
+
+            }
+
+        }
+    }
+}
